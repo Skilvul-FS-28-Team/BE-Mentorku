e050c61976a890131320413a282efe9e1a8e42a1
Hellodyonz <hellodyonz@gmail.com>
2023-11-22 11:43:41 +0700

    connection success

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
-{
+module.exports = {
   "development": {
     "username": "root",
-    "password": null,
-    "database": "database_development",
+    "password": "dyonz123",
+    "database": "Mentorku_BE",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
